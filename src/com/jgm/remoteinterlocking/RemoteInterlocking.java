c00ad042627d6dc72b082df3c3a7ac23482c5f86
joth <joth@jonathans-imac>
2016-09-30 13:35:10 +0100

    Updated by Jonathan Moss

---
+++ src/com/jgm/remoteinterlocking/RemoteInterlocking.java
@@ -0,0 +1,14 @@
+package com.jgm.remoteinterlocking;
+
+/**
+ *
+ * @author Jonathan Moss
+ * @version v1.0 October 2016
+ */
+public class RemoteInterlocking {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/com/jgm/remoteinterlocking/connections/OutgoingMessages.java
@@ -0,0 +1,9 @@
+package com.jgm.remoteinterlocking.connections;
+/**
+ *
+ * @author Jonathan Moss
+ * @version v1.0 October 2016
+ */
+public class OutgoingMessages {
+
+}
