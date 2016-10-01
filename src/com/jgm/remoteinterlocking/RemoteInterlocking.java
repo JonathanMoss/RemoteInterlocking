ea75aae1e9522ec29942e9ace76ba04c84d3b610
joth <joth@jonathans-imac>
2016-10-01 13:55:08 +0100

    Updated by Jonathan Moss

--- src/com/jgm/remoteinterlocking/RemoteInterlocking.java
+++ src/com/jgm/remoteinterlocking/RemoteInterlocking.java
@@ -7,8 +7,21 @@ package com.jgm.remoteinterlocking;
  */
 public class RemoteInterlocking {
 
-   
+    private static String riIdentity;
     public static void main(String[] args) {
+        // 1) Obtain the remote interlocking ID from the command line
+        if (args.length > 0) {
+            
+        } else {
+            
+        }
+    }
+    
+    /**
+     * This method outputs to the console and Data Logger as required.
+     * @param msg 
+     */
+    public void sendStatusMessage(String msg) {
         
     }
     
